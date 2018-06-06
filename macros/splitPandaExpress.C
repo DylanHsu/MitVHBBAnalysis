37cd792d1d75bb3b7fd51d1ce76ab06f5cbc2931
Dylan George Hsu <dghsu@mit.edu>
2018-06-06 15:18:03 -0400

    update splitter macro to handle multiple cycle numbers from merging

--- macros/splitPandaExpress.C
+++ macros/splitPandaExpress.C
@@ -1,8 +1,40 @@
 // does not compile in Aclic, use in CINT only
-void splitPandaExpress(string inputFile, Long64_t nEvtsPerFile=100000) {
+void splitPandaExpress(string inputFile, Long64_t nEvtsPerFile=500000) {
   printf("Opening file \"%s\" and splitting it into files with %lld events...\n", inputFile.c_str(), nEvtsPerFile);
-  TFile *f = TFile::Open(inputFile.c_str(),"read"); assert(f);
-  TTree *events = (TTree*)f->Get("events"); assert(events);
+  TFile *f = TFile::Open(inputFile.c_str(),"update"); assert(f);
+  TTree *events=0, *treebuffer;
+  
+  // Drop extra cycle numbers so rooteventselector can do its job
+  printf("\tLooking for extra cycle numbers to drop...\n");
+  TIter keyList(f->GetListOfKeys());
+  vector<short> extraCycleNumbers; short highestCycleNumber=-1;
+  TKey *key;
+  while ((key = (TKey*)keyList())) {
+    TClass *cl = gROOT->GetClass(key->GetClassName());
+    if (!cl->InheritsFrom("TTree")) continue;
+    treebuffer=(TTree*)key->ReadObj();
+    if(strcmp(treebuffer->GetName(),"events")!=0) continue;
+    short cycleNumber=key->GetCycle();
+    printf("\t%s;%d\n",treebuffer->GetName(),cycleNumber);
+    if(cycleNumber > highestCycleNumber) {
+      // replace the highest cycle number, putting the previous highest in the list of extras if real
+      if(highestCycleNumber>0) extraCycleNumbers.push_back(highestCycleNumber);
+      highestCycleNumber=cycleNumber;
+    } else {
+      // this is an extra cycle number
+      extraCycleNumbers.push_back(cycleNumber);
+    }
+  }
+  std::cout << "Extra cycle numbers: ";
+  for (std::vector<short>::const_iterator i = extraCycleNumbers.begin(); i != extraCycleNumbers.end(); ++i)
+    std::cout << *i << ", ";
+  std::cout << std::endl;
+  for (std::vector<short>::const_iterator i = extraCycleNumbers.begin(); i != extraCycleNumbers.end(); ++i)
+    f->Delete(Form("events;%d",*i));
+  // Done dropping extra cycle numbers
+ 
+  
+  events = (TTree*)f->Get(Form("events;%d",highestCycleNumber)); assert(events);
   Long64_t nEntries = events->GetEntries();
   unsigned nSplit = ceil( float(nEntries) / nEvtsPerFile);
   f->Close();
